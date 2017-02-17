889981ce5836ef81426cbacddfa415a3b46050bb
Cristian <me@cristian.io>
2017-02-17 09:56:51 -0800

    Added license information

--- rx-bloomd-client/src/test/java/bloomd/ShampooRule.java
+++ rx-bloomd-client/src/test/java/bloomd/ShampooRule.java
@@ -4,6 +4,12 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+/**
+ * Apply to flaky tests as needed
+ * <p>
+ * Apache 2 license
+ * https://gist.github.com/JakeWharton/7fe7deb1f7f4a795c120
+ */
 public final class ShampooRule implements TestRule {
     private final int iterations;
 
@@ -12,9 +18,11 @@ public final class ShampooRule implements TestRule {
         this.iterations = iterations;
     }
 
-    @Override public Statement apply(final Statement base, Description description) {
+    @Override
+    public Statement apply(final Statement base, Description description) {
         return new Statement() {
-            @Override public void evaluate() throws Throwable {
+            @Override
+            public void evaluate() throws Throwable {
                 for (int i = 0; i < iterations; i++) {
                     base.evaluate();
                 }
